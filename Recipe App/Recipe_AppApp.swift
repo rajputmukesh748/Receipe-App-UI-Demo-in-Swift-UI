552a6f2799002291f421cfebd1b8f9ee1dd4772b
Mukesh Kumar <mukesh.rajput@appinventiv.com>
2024-10-19 14:50:40 +0530

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by apple on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recipe AppTests/Recipe_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppTests.swift
+//  Recipe AppTests
+//
+//  Created by apple on 19/10/24.
+//
+
+import Testing
+@testable import Recipe_App
+
+struct Recipe_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
